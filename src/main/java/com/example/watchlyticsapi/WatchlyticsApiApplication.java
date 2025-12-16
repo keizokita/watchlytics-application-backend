5b028c8cd99a9b8b7211d3b8f41e2a64593d4298
keizo.kita <leonardo.kita@amplaito.com.br>
2025-12-16 16:13:50 -0300

    feat: Initial commit!

---
+++ src/main/java/com/example/watchlyticsapi/WatchlyticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchlyticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlyticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlyticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/watchlyticsapi/WatchlyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchlyticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
